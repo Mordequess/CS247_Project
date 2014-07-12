47ee4b4a368e96a3c7b791b6207777d03ae0129f
Mordequess <t.babaran@gmail.com>
2014-07-12 15:45:11 -0400

    update deckgui

--- Code/DeckGUI.cpp
+++ Code/DeckGUI.cpp
@@ -15,21 +15,36 @@ using std::transform;
 
 // Sets up an array of the Portable Network Graphics (PNG) file names that contain the necessary card images.
 // The deck will load the contents into pixel buffers for later use.
-const char * image_names[] = {   
-	// Set up NINE of {DIAMOND,CLUB,HEART,SPADE}
-	"cards_png/0_0.png", "cards_png/0_1.png", "cards_png/0_2.png", "cards_png/0_3.png",
-	// Set up TEN of {DIAMOND,CLUB,HEART,SPADE}
-	"cards_png/1_0.png", "cards_png/1_1.png", "cards_png/1_2.png", "cards_png/1_3.png",
-	// Set up JACK of {DIAMOND,CLUB,HEART,SPADE}
-	"cards_png/2_0.png", "cards_png/2_1.png", "cards_png/2_2.png", "cards_png/2_3.png",
-	// Set up QUEEN of {DIAMOND,CLUB,HEART,SPADE}
-	"cards_png/3_0.png", "cards_png/3_1.png", "cards_png/3_2.png", "cards_png/3_3.png",
-	// Set up KING of {DIAMOND,CLUB,HEART,SPADE}
-	"cards_png/4_0.png", "cards_png/4_1.png", "cards_png/4_2.png", "cards_png/4_3.png",
-	// Set up ACE of {DIAMOND,CLUB,HEART,SPADE}
-	"cards_png/5_0.png", "cards_png/5_1.png", "cards_png/5_2.png", "cards_png/5_3.png",
+const char * image_names[] = {
+	// Set up A of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_0.png", "cards_png/1_0.png", "cards_png/2_0.png", "cards_png/3_0.png",
+	// Set up 2 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_1.png", "cards_png/1_1.png", "cards_png/2_1.png", "cards_png/3_1.png",
+	// Set up 3 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_2.png", "cards_png/1_2.png", "cards_png/2_2.png", "cards_png/3_2.png",
+	// Set up 4 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_3.png", "cards_png/1_3.png", "cards_png/2_3.png", "cards_png/3_3.png",
+	// Set up 5 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_4.png", "cards_png/1_4.png", "cards_png/2_4.png", "cards_png/3_4.png",
+	// Set up 6 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_5.png", "cards_png/1_5.png", "cards_png/2_5.png", "cards_png/3_5.png",
+	// Set up 7 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_6.png", "cards_png/1_6.png", "cards_png/2_6.png", "cards_png/3_6.png",
+	// Set up 8 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_7.png", "cards_png/1_7.png", "cards_png/2_7.png", "cards_png/3_7.png",
+	// Set up 9 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_8.png", "cards_png/1_8.png", "cards_png/2_8.png", "cards_png/3_8.png",
+	// Set up 10 of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_9.png", "cards_png/1_9.png", "cards_png/2_9.png", "cards_png/3_9.png",
+	// Set up J of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_10.png", "cards_png/1_10.png", "cards_png/2_10.png", "cards_png/3_10.png",
+	// Set up Q of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_11.png", "cards_png/1_11.png", "cards_png/2_11.png", "cards_png/3_11.png",
+	// Set up K of {CLUB,DIAMOND,HEART,SPADE}
+	"cards_png/0_12.png", "cards_png/1_12.png", "cards_png/2_12.png", "cards_png/3_12.png",
+
 	// Set up the back of a card for a place holder/null card
-	"cards_png/back_1.png"
+	"cards_png/nothing.png"
 }; 
 
 // Loads the image from the specified file name into a pixel buffer.
